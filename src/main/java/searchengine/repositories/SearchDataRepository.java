1e602b6cea3a2d5d18219060269b2b1a8c63d553
nikitinaak <nikit.alexandra@mail.ru>
2023-05-29 16:02:54 +0300

    Removed unnecessary import

--- src/main/java/searchengine/repositories/SearchDataRepository.java
+++ src/main/java/searchengine/repositories/SearchDataRepository.java
@@ -2,8 +2,6 @@ package searchengine.repositories;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
