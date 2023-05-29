703b802a907f295de2039b1ec07859095d7a2464
nikitinaak <nikit.alexandra@mail.ru>
2023-05-29 15:59:44 +0300

    Add Pageable

--- src/main/java/searchengine/dto/searching/SearchResponse.java
+++ src/main/java/searchengine/dto/searching/SearchResponse.java
@@ -1,13 +1,14 @@
 package searchengine.dto.searching;
 
 import lombok.Data;
-import org.springframework.data.domain.Page;
 import searchengine.dto.responses.Response;
 import searchengine.model.SearchData;
+
+import java.util.List;
 
 @Data
 public class SearchResponse implements Response {
     private boolean result;
     private int count;
-    private Page<SearchData> data;
+    private List<SearchData> data;
 }
--- src/main/java/searchengine/repositories/SearchDataRepository.java
+++ src/main/java/searchengine/repositories/SearchDataRepository.java
@@ -3,6 +3,7 @@ package searchengine.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import searchengine.model.SearchData;
 
 @Repository
 public interface SearchDataRepository extends PagingAndSortingRepository<SearchData, Integer> {
+
     Page<SearchData> findAll(Pageable pageable);
 
     @Query(value = "SELECT COUNT(*) FROM `search_data`", nativeQuery = true)
